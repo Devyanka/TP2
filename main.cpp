ad107556248a894635dd1522e562ac51573c7c8a
vivomisar <vivomisar.eg@gmail.com>
2024-12-12 18:18:59 +0300

    Сделано меню первого задания

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
+#include "TrainList.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    TrainList trainList;
+    int choice;
+
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add Train\n";
+        cout << "2. Print All Trains\n";
+        cout << "3. Find Trains by Destination\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                Train train;
+                cin >> train;
+                trainList.addTrain(train);
+                break;
+            }
+            case 2:
+                trainList.printTrains();
+                break;
+            case 3: {
+                string destination;
+                cout << "Enter destination: ";
+                cin >> destination;
+                trainList.findTrainsByDestination(destination);
+                break;
+            }
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 4);
 
     return 0;
 }
